1b2238dc90227f2b7cf3c61ca6b3cfefd0813474
Umang Tiwari <umang.tiwari@payu.in>
2022-10-27 18:29:02 +0530

    Initial Project Skeleton created

---
+++ src/main/java/umang/springframework/petclinicspring/PetClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package umang.springframework.petclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringApplication.class, args);
+    }
+
+}
